0aee203d2dc3ea264fcacc09ac3c798af6d36067
yusiyuan <yusiyaun@zhuojianchina.com>
2018-11-02 16:49:19 +0800

    update vue 框架初始化 上传原型设计

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueSocketIo from 'vue-socket.io'
+import socketIo from 'socket.io-client'
 
+// Vue.use(VueSocketIo, socketIo('http://localhost:3000'))
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
