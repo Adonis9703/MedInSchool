23b4cafb5cc493c65e845cd0cb7e901fb1247ce2
yusiyuan <yusiyaun@zhuojianchina.com>
2018-11-13 17:27:53 +0800

    UPDATE README 私聊 数据库设计 socketId与userId 映射

--- nodetest.sql
+++ nodetest.sql
@@ -11,29 +11,48 @@
  Target Server Version : 80013
  File Encoding         : 65001
 
- Date: 06/11/2018 17:29:30
+ Date: 13/11/2018 17:27:30
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for userinfo
+-- Table structure for socketinfo
 -- ----------------------------
-DROP TABLE IF EXISTS `userinfo`;
-CREATE TABLE `userinfo`  (
-  `id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+DROP TABLE IF EXISTS `socketinfo`;
+CREATE TABLE `socketinfo`  (
+  `userId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `socketId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of socketinfo
+-- ----------------------------
+INSERT INTO `socketinfo` VALUES ('5150510116', 'PajXa3oqFPflXdJ6AAAB');
+
+-- ----------------------------
+-- Table structure for studentinfo
+-- ----------------------------
+DROP TABLE IF EXISTS `studentinfo`;
+CREATE TABLE `studentinfo`  (
   `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `studentId` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `idCard` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `sex` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  `tel` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `bloodType` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `allergy` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `otherInfo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`studentId`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of userinfo
+-- Records of studentinfo
 -- ----------------------------
-INSERT INTO `userinfo` VALUES ('1', 'Alex', '5150510116', '330821199703104213', '男', '123456');
+INSERT INTO `studentinfo` VALUES ('Alex', '5150510114', '330821199703104213', '男', '123456', '15869106432', 'A', '暂无过敏', '暂无');
+INSERT INTO `studentinfo` VALUES ('Alexia', '5150510115', '330821199703104213', '男', '123456', '15869106432', 'A', '暂无过敏', '暂无');
+INSERT INTO `studentinfo` VALUES ('Alex', '5150510116', '330821199703104213', '男', '123456', '15869106432', 'A', '暂无过敏', '暂无');
 
 SET FOREIGN_KEY_CHECKS = 1;
