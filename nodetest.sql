ec7b9638ed465b0f004056755688c323477f46b3
yusiyuan <yusiyaun@zhuojianchina.com>
2018-11-23 16:30:36 +0800

    UPDATE 数据库更新

--- nodetest.sql
+++ nodetest.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80013
  File Encoding         : 65001
 
- Date: 13/11/2018 17:27:30
+ Date: 23/11/2018 16:30:23
 */
 
 SET NAMES utf8mb4;
@@ -23,13 +23,17 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `socketinfo`;
 CREATE TABLE `socketinfo`  (
   `userId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `socketId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+  `socketId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`userId`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of socketinfo
 -- ----------------------------
-INSERT INTO `socketinfo` VALUES ('5150510116', 'PajXa3oqFPflXdJ6AAAB');
+INSERT INTO `socketinfo` VALUES ('5150510110', 'XigAvuA1kypbfpyBAAAB', 'offline');
+INSERT INTO `socketinfo` VALUES ('5150510115', '9V3OHC27HVwK4QEAAAAA', 'a');
+INSERT INTO `socketinfo` VALUES ('5150510116', 'ItvWWZMEVFN4ZiOQAAAD', 'online');
 
 -- ----------------------------
 -- Table structure for studentinfo
@@ -51,8 +55,11 @@ CREATE TABLE `studentinfo`  (
 -- ----------------------------
 -- Records of studentinfo
 -- ----------------------------
-INSERT INTO `studentinfo` VALUES ('Alex', '5150510114', '330821199703104213', '男', '123456', '15869106432', 'A', '暂无过敏', '暂无');
+INSERT INTO `studentinfo` VALUES ('Alex', '5150510110', '330821199703104213', '男', '123456', '15869106432', 'A', '暂无过敏', '暂无');
+INSERT INTO `studentinfo` VALUES ('Alex', '5150510111', '330821199703104213', '男', '123456', '15869106432', 'D', '暂无过敏', '暂无');
+INSERT INTO `studentinfo` VALUES ('Alex', '5150510113', '330821199703104213', '男', '123456', '15869106432', 'C', '暂无过敏', '暂无');
+INSERT INTO `studentinfo` VALUES ('Alex', '5150510114', '330821199703104213', '男', '123456', '15869106432', 'B', '暂无过敏', '暂无');
 INSERT INTO `studentinfo` VALUES ('Alexia', '5150510115', '330821199703104213', '男', '123456', '15869106432', 'A', '暂无过敏', '暂无');
-INSERT INTO `studentinfo` VALUES ('Alex', '5150510116', '330821199703104213', '男', '123456', '15869106432', 'A', '暂无过敏', '暂无');
+INSERT INTO `studentinfo` VALUES ('Alex', '5150510116', '330821199703104213', '男', '123456', '15869106432', 'B', '暂无过敏', '暂无');
 
 SET FOREIGN_KEY_CHECKS = 1;
