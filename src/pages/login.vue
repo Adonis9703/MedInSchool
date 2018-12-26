13a9ce8ed2de8867d6992cb5e5d6b3a9974b1f89
yusiyuan <yusiyaun@zhuojianchina.com>
2018-12-26 17:45:50 +0800

    UPDATE 登陆页编写

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    <img :src="playground" class="width100 height100 fixed z-index-1">
+  <div class="back">
     <div style="padding-top: 300px">
       <el-row>
         <el-col :lg="9" :md="10" :offset="5">
           <div class="color-white bold title">浙科医务室</div>
         </el-col>
         <el-col :lg="6" :md="3">
-          <div class="card-panel shadow margin-left60">asdas</div>
+          <div class="card-panel shadow margin-left60 paddingX20 padding20X">
+            <div class="">
+              <input class="input-dark block paddingX10 padding10X font-size-4" placeholder="工号"/>
+              <input class="input-dark block paddingX10 padding10X font-size-4 margin-top20" placeholder="密码" type="password"/>
+            </div>
+            <div class="btn-primary margin-top10">登陆</div>
+          </div>
         </el-col>
       </el-row>
     </div>
@@ -32,10 +37,20 @@
 </script>
 
 <style lang="scss" scoped>
+  .back {
+    width: 100%;
+    position: absolute;
+    height: 100%;
+    background-image: url("../assets/images/back/uni_playground.jpg")
+  }
+
   .title {
     display: inline-block;
     letter-spacing: 15px;
     font-family: "Microsoft YaHei UI Light";
     font-size: 80px;
+  }
+  input{
+    max-width: 200px;
   }
 </style>
